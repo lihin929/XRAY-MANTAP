6e4885aa9756c7afa02b0d030cd3faa22fe8ddc1
lihin929 <85750131+lihin929@users.noreply.github.com>
2023-09-24 21:02:36 +0700

    Add files via upload

---
+++ domain/cf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+clear
+read -rp "Input Your Domain For This Server :" -e DOMAIN
+echo "Host : $SUB_DOMAIN"
+echo $DOMAIN > /root/domain
+# / / Make Main Directory
+mkdir -p /etc/xray
+cp /root/domain /etc/xray
+rm -f /root/cf.sh
